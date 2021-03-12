645faf530c65dac74c1f201602eb99192c421d9f
lee <loyalpartner@163.com>
2021-03-12 13:48:34 +0800

    add http restful support

--- main.go
+++ main.go
@@ -2,22 +2,60 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"log"
+	"net"
+	"net/http"
 
-	hellowordpb "github.com/loyalpartner/grpc-gateway-example/proto/helloworld"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"google.golang.org/grpc"
+
+	helloworldpb "github.com/loyalpartner/grpc-gateway-example/proto/helloworld"
 )
 
-type server struct{}
-
-func NewServer() *server {
-	return &server{}
+type server struct {
+	// https://github.com/grpc/grpc-go/issues/3794
+	helloworldpb.UnimplementedGreeterServer
 }
 
-// func (s *server) SayHello(ctx context.Context, in *hello)
-func (s *server) SayHello(cxt context.Context, in *hellowordpb.HelloRequest) (*helloworldpb.HelloReply, error) {
+// func NewServer() *server {
+// 	return &server{}
+// }
 
+func (s *server) SayHello(cxt context.Context, in *helloworldpb.HelloRequest) (*helloworldpb.HelloReply, error) {
+	return &helloworldpb.HelloReply{Message: in.Name + " world"}, nil
 }
 
 func main() {
+	lis, err := net.Listen("tcp", ":9999")
+	if err != nil {
+		log.Fatalln("Failed to listen", err)
+	}
 
+	s := grpc.NewServer()
+	helloworldpb.RegisterGreeterServer(s, &server{})
+
+	log.Println("Serving gRPC on 0.0.0.0:9999")
+	go func(){
+		log.Fatal(s.Serve(lis))
+	}()
+
+	conn, err := grpc.DialContext(context.Background(), "0.0.0.0:9999", grpc.WithBlock(), grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatal("Failed to dial server:", err)
+	}
+
+	gwmux := runtime.NewServeMux()
+	err = helloworldpb.RegisterGreeterHandler(context.Background(), gwmux, conn)
+	if err != nil {
+		log.Fatalln("Failed to register gateway:", err)
+	}
+
+	gwServer := &http.Server{
+		Addr: ":8999",
+		Handler: gwmux,
+	}
+
+	log.Println("Serving gRPC-Gateway on http://0.0.0.0:8999")
+	log.Fatalln(gwServer.ListenAndServe())
 }
